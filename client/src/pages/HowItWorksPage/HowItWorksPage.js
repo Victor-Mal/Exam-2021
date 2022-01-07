823c204ac2eb13e2125a75dcd4eb7e0ce2dfe96c
Victor Mal <VictorianusVelikyu@gmail.com>
2022-01-08 01:11:39 +0200

    Add raw construction

---
+++ client/src/pages/HowItWorksPage/HowItWorksPage.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import CONSTANTS from '../../constants';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import styles from './HowItWorksPage.module.sass';
+
+const HowItWorksPage = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <div>
+          <div>
+            <h1>How Does Squadhelp Work?</h1>
+            <p>
+              Squadhelp helps you come up with a great name for your business by
+              combining the power of crowdsourcing with sophisticated technology
+              and Agency-level validation services.
+            </p>
+          </div>
+          <img 
+           src={`${CONSTANTS.STATIC_IMAGES_PATH}/svg/app-user.svg`}
+           alt="app user"
+          />
+        </div>
+        <div>
+          <h2>3 Ways To Use Squadhelp</h2>
+          <p>
+            Squadhelp offers 3 ways to get you a perfect name for your business.
+          </p>
+          <div>
+            <div>
+              <h3>Launch a Contest</h3>
+              <p>
+                Work with hundreds of creative experts to get custom name
+                suggestions for your business or brand. All names are
+                auto-checked for URL availability.
+              </p>
+              <a href="http://www.google.com">Launch a Contest</a>
+            </div>
+            <div>
+              <h3>Explore Names For Sale</h3>
+              <p>
+                Our branding team has curated thousands of pre-made names that
+                you can purchase instantly. All names include a matching URL and
+                a complimentary Logo Design
+              </p>
+              <a href="http://www.google.com">Explore Names For Sale</a>
+            </div>
+            <div>
+              <h3>Agency-level Managed Contests</h3>
+              <p>
+                Our Managed contests combine the power of crowdsourcing with the
+                rich experience of our branding consultants. Get a complete
+                agency-level experience at a fraction of Agency costs
+              </p>
+              <a href="http://www.google.com">Launch a Contest</a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2>How Do Naming Contests Work?</h2>
+          <div>
+          <img 
+           src={`${CONSTANTS.STATIC_IMAGES_PATH}/svg/house-agency.svg`}
+           alt="house-agency"
+          />
+            <ul>
+              <li>
+                <span>1</span>
+                <p>
+                  Fill out your Naming Brief and begin receiving name ideas in
+                  minutes
+                </p>
+              </li>
+              <li>
+                <span>2</span>
+                <p>
+                  Rate the submissions and provide feedback to creatives.
+                  Creatives submit even more names based on your feedback.
+                </p>
+              </li>
+              <li>
+                <span>3</span>
+                <p>
+                  Our team helps you test your favorite names with your target
+                  audience. We also assist with Trademark screening.
+                </p>
+              </li>
+              <li>
+                <span>4</span>
+                <p>Pick a Winner. The winner gets paid for their submission.</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default connect(null)(HowItWorksPage);
