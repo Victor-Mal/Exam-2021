3e961814e275df057e6fac87f4f68353bed9d27d
Victor Mal <VictorianusVelikyu@gmail.com>
2022-01-09 00:53:47 +0200

    Add change to styles and adaptive

--- client/src/pages/HowItWorksPage/HowItWorksPage.js
+++ client/src/pages/HowItWorksPage/HowItWorksPage.js
@@ -12,7 +12,7 @@ const HowItWorksPage = () => {
       <Header />
       <div className={styles.container}>
         <div className={styles.firstBlock}>
-          <div>
+          <div className={styles.header}>
             <h1>How Does Squadhelp Work?</h1>
             <p>
               Squadhelp helps you come up with a great name for your business by
